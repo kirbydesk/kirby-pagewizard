e224cf39368929a9f5ea1d9f02205fecb6471026
Christian Fickinger <chrfickinger@users.noreply.github.com>
2025-10-24 13:46:39 +0200

    Remove auto content generated

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	/* -------------- Extensions --------------*/
   'areas' 				=> require_once 'src/extensions/areas.php',
 	'blueprints'		=> require_once 'src/extensions/blueprints.php',
-	'hooks' 				=> require_once 'src/extensions/hooks.php',
 	'snippets'			=> require_once 'src/extensions/snippets.php',
 	'templates'			=> require_once 'src/extensions/templates.php',
   'translations' 	=> require_once 'src/extensions/translations.php',
