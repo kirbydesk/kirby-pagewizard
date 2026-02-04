b63f0921355a298178d1e349df61972621fd64ab
Christian Fickinger <mail@christianfickinger.de>
2026-02-04 12:02:54 +0100

    Work in progress

---
+++ src/helpers/blocks/theme.php
@@ -0,0 +1,40 @@
+<?php
+
+class pwTheme
+{
+	public static function options(string $blockType, array $blockDefaults = []): array
+	{
+		// 1. Start with block defaults
+		$defaults = [
+			'theme' => 'default',
+		];
+
+		// 2. Merge with block-specific defaults from blueprint
+		$defaults = array_merge($defaults, $blockDefaults);
+
+		// 3. Override with config values if they exist
+		$config = kirby()->option('kirbydesk.pagewizard.kirbyblocks.' . $blockType, []);
+		if (!empty($config) && isset($config['theme'])) {
+			$defaults['theme'] = $config['theme'];
+		}
+
+		return [
+			'label'  => 'pw.tab.theme',
+			'fields' => [
+				'headlineTheme' => [
+					'extends' => 'pagewizard/headlines/theme',
+				],
+				'theme' => [
+					'type'    => 'select',
+					'label'   => 'pw.field.theme',
+					'options' => [
+						['value' => 'default', 'text' => 'pw.option.default'],
+						['value' => 'variant', 'text' => 'pw.option.variant'],
+					],
+					'default' => $defaults['theme'],
+					'help'    => 'pw.field.theme.help'
+				]
+			]
+		];
+	}
+}
