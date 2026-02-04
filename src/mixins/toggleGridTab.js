cbde2cc9ba112c00dcaa5c93fcc0468d94f0bf43
Christian Fickinger <mail@christianfickinger.de>
2026-02-04 13:03:40 +0100

    Test

--- src/mixins/toggleGridTab.js
+++ src/mixins/toggleGridTab.js
@@ -23,10 +23,27 @@ export default {
 
   methods: {
     setGridDrawerClass() {
-      const drawers = document.querySelectorAll('.k-drawer.k-form-drawer');
-      drawers.forEach(drawer => {
-        drawer.classList.toggle('hide-grid-tab', this.content.togglegrid === false);
-      });
+      console.log('toggleGridTab.js: this.id', this.id);
+      // Try to find the drawer for this block by data-block-id
+      let drawer = document.querySelector('.k-drawer.k-form-drawer[data-block-id="' + this.id + '"]');
+      console.log('toggleGridTab.js: drawer before', drawer);
+      // If not found, try to set the attribute on the active drawer (only needed the first time)
+      if (!drawer) {
+        const activeDrawer = document.querySelector('.k-drawer.k-form-drawer[aria-current="true"]');
+        if (activeDrawer) {
+          activeDrawer.setAttribute('data-block-id', this.id);
+          drawer = activeDrawer;
+        }
+      }
+      console.log('toggleGridTab.js: drawer after', drawer);
+      if (drawer) {
+        const isOff = this.toggleGrid === false || this.toggleGrid === 'false';
+        console.log('toggleGridTab.js: toggleGrid value', this.toggleGrid, 'isOff:', isOff);
+        drawer.classList.toggle('hide-grid-tab', isOff);
+        console.log('toggleGridTab.js: hide-grid-tab gesetzt:', drawer.classList.contains('hide-grid-tab'), drawer);
+      } else {
+        console.warn('toggleGridTab.js: Kein Drawer gefunden für id', this.id);
+      }
     }
   }
 };
