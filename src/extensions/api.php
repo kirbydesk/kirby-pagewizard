1dd041cf462be66f0eb4b58f29b1c9dc587538e1
Christian Fickinger <mail@christianfickinger.de>
2026-02-17 15:07:09 +0100

    WIP

--- src/extensions/api.php
+++ src/extensions/api.php
@@ -8,6 +8,16 @@ return [
 			'action'  => function (string $blockType) {
 				return ['settings' => pwConfig::settings($blockType)];
 			}
+		],
+		[
+			'pattern' => 'pagewizard/colors',
+			'action'  => function () {
+				$colorsFile = kirby()->root('temp') . '/pw-colors.json';
+				if (file_exists($colorsFile)) {
+					return json_decode(file_get_contents($colorsFile), true);
+				}
+				return ['default' => [], 'variant' => []];
+			}
 		]
 	]
 ];
