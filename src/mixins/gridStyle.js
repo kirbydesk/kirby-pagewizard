843083653ce92e8f9883b5c8000cece7c47bc75c
Christian Fickinger <mail@christianfickinger.de>
2026-03-06 11:37:39 +0100

    Version bump

--- src/mixins/gridStyle.js
+++ src/mixins/gridStyle.js
@@ -1,16 +1,17 @@
 export default {
 	computed: {
 		gridVars() {
-      return {
-        '--grid-start-sm': (Number(this.content.gridoffsetsm)),
-        '--grid-span-sm': Number(this.content.gridsizesm),
-        '--grid-start-md': (Number(this.content.gridoffsetmd)),
-        '--grid-span-md': Number(this.content.gridsizemd),
-        '--grid-start-lg': (Number(this.content.gridoffsetlg)),
-        '--grid-span-lg': Number(this.content.gridsizelg),
-        '--grid-start-xl': (Number(this.content.gridoffsetxl)),
-        '--grid-span-xl': Number(this.content.gridsizexl),
-      };
-    }
-  }
+			const offset = (val) => { const n = Number(val); return n === 0 ? 'auto' : n + 1; };
+			return {
+				'--grid-start-sm': offset(this.content.gridoffsetsm),
+				'--grid-span-sm':  Number(this.content.gridsizesm),
+				'--grid-start-md': offset(this.content.gridoffsetmd),
+				'--grid-span-md':  Number(this.content.gridsizemd),
+				'--grid-start-lg': offset(this.content.gridoffsetlg),
+				'--grid-span-lg':  Number(this.content.gridsizelg),
+				'--grid-start-xl': offset(this.content.gridoffsetxl),
+				'--grid-span-xl':  Number(this.content.gridsizexl),
+			};
+		}
+	}
 };
